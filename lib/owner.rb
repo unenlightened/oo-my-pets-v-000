aac3925d09465cf7c0ea23d7505c161b17874dcc
Chely Ho <adept_inept@yahoo.com>
2018-02-13 00:35:27 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -27,38 +27,38 @@ class Owner
 
   def buy_fish(name)
     fish = Fish.new(name)
-    @pets[:fishes] << fish
+    pets[:fishes] << fish
   end
 
   def buy_cat(name)
     cat = Cat.new(name)
-    @pets[:cats] << cat
+    pets[:cats] << cat
   end
 
   def buy_dog(name)
     dog = Dog.new(name)
-    @pets[:dogs] << dog
+    pets[:dogs] << dog
   end
 
   def walk_dogs
-    @pets[:dogs].each {|dog| dog.mood = "happy"}
+    pets[:dogs].each {|dog| dog.mood = "happy"}
   end
 
   def play_with_cats
-    @pets[:cats].each {|cat| cat.mood = "happy"}
+    pets[:cats].each {|cat| cat.mood = "happy"}
   end
 
   def feed_fish
-    @pets[:fishes].each {|fish| fish.mood = "happy"}
+    pets[:fishes].each {|fish| fish.mood = "happy"}
   end
 
   def sell_pets
-    @pets.each do |species, pet|
-      @pets[species].delete_if {|pet| pet.mood = "nervous"}  # can also use shift or set pets[:species] = []
+    pets.each do |species, pet|
+      pets[species].delete_if {|pet| pet.mood = "nervous"}  # can also use shift or set pets[:species] = []
     end
   end
 
   def list_pets  # "I have 2 fish, 3 dog(s), and 1 cat(s)."
-    "I have #{@pets[:fishes].count} fish, #{@pets[:dogs].count} dog(s), and #{@pets[:cats].count} cat(s)."
+    "I have #{pets[:fishes].count} fish, #{pets[:dogs].count} dog(s), and #{pets[:cats].count} cat(s)."
   end
 end
